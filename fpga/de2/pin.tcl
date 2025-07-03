2aef0a4638beaef294e89ff00ec7ed1ec2e7342b
Heqing Huang <feipenghhq@gmail.com>
2025-07-02 23:03:43 -0700

    Create a FPGA demo program in DE2 FPGA board
    Same loopback demo as Arty FPGA board

---
+++ fpga/de2/pin.tcl
@@ -0,0 +1,11 @@
+# KEY
+set_location_assignment PIN_G26 -to KEY
+
+# CLOCK
+set_location_assignment PIN_N2 -to CLOCK_50
+
+# UART
+set_location_assignment PIN_C25 -to UART_RXD
+set_location_assignment PIN_B25 -to UART_TXD
+
+
